96ddbd2167882609fcdf1aabd59ccb5daccdfbfd
m-shimao <shimao.ma@gmail.com>
2017-12-16 17:15:37 +0900

    :+1:jsとcss追加

--- speech_recognition/web_speech/js/api.js
+++ speech_recognition/web_speech/js/api.js
@@ -1,4 +1,4 @@
-var recognition = new SpeechRecognition();
+var recognition = new webkitSpeechRecognition();
   recognition.onresult = function(event) {
   if (event.results.length > 0) {
     q.value = event.results[0][0].transcript;
